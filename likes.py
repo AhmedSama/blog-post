7f5cc9ead8d4c7e3bde4c0ee3ba25744d191599f
AhmedSama <nwnwlala@yahoo.com>
2021-09-16 01:23:16 +0300

    adding likes feature

---
+++ likes.py
@@ -0,0 +1,69 @@
+import sqlite3
+from os import path
+
+ROOT = path.dirname(path.realpath(__file__))
+
+
+def make_db():
+    conn = sqlite3.connect("data.db")
+    cur = conn.cursor()
+    cur.execute("""create table likes(
+        post_id integer not null,
+        user_id integer not null
+    )""")
+    conn.commit()
+    conn.close()
+
+
+def add(post_id, user_id):
+    conn = sqlite3.connect("data.db")
+    cur = conn.cursor()
+    cur.execute("insert into likes( post_id, user_id) values(?,?)",
+                (post_id, user_id))
+    conn.commit()
+    conn.close()
+
+
+def get_all():
+    conn = sqlite3.connect("data.db")
+    cur = conn.cursor()
+    cur.execute("select rowid,* from likes")
+    conn.commit()
+    data = cur.fetchall()
+    conn.close()
+    return data
+
+
+def delete(id_):
+    conn = sqlite3.connect("data.db")
+    cur = conn.cursor()
+    cur.execute("delete from likes where rowid = ?", (id_,))
+    conn.commit()
+    conn.close()
+def count_likes(post_id):
+    conn = sqlite3.connect("data.db")
+    cur = conn.cursor()
+    cur.execute("select count(post_id) from likes where post_id = ?", (post_id,))
+    conn.commit()
+    data = cur.fetchone()
+    conn.close()
+    return data[0]
+
+def get_by(post_id,user_id):
+    conn = sqlite3.connect("data.db")
+    cur = conn.cursor()
+    cur.execute(
+        "select * from likes where post_id=? and user_id = ?", (post_id,user_id))
+    conn.commit()
+    data = cur.fetchall()
+    conn.close()
+    return data
+
+
+
+
+
+
+
+
+
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request,redirect,url_for,session
-import models,posts_db,comments
+import models,posts_db,comments,likes
 from os import path,mkdir
 
 ROOT = path.dirname(path.abspath(__file__))
@@ -92,10 +92,11 @@ def idea(post_id):
         id_ = session.get("user")[0]
         post = posts_db.get_by_id(post_id)
         post_comments = comments.get_by_id(post_id)
-        return render_template("idea.html", username=session.get("user")[1].capitalize(), post=post, title=session.get("user")[2].capitalize(), bio=session.get("user")[3], img=session.get("user")[4], post_comments=post_comments)
+        likes_number = likes.count_likes(post_id)
+        return render_template("idea.html", username=session.get("user")[1].capitalize(), post=post, title=session.get("user")[2].capitalize(), bio=session.get("user")[3], img=session.get("user")[4], post_comments=post_comments, likes_number=likes_number)
     return render_template("login.html")
 
-
+# comminting feature
 @app.route("/add_comment/<int:post_id>",methods=["POST"])
 def add_comment(post_id):
     if session.get("user"):
@@ -104,6 +105,18 @@ def add_comment(post_id):
         comments.add(comment,post_id,id_)
         return redirect(url_for("idea",post_id=post_id))
     return render_template("login.html")
+
+# liking feature
+@app.route("/add_like/<int:post_id>", methods=["POST"])
+def add_like(post_id):
+    if session.get("user"):
+        id_ = session.get("user")[0]
+        if likes.get_by(post_id,id_) == []:
+            likes.add(post_id,id_)
+        
+        return redirect(url_for("idea", post_id=post_id))
+    return render_template("login.html")
+
 
 @app.route("/post",methods=["GET","POST"])
 def post():
